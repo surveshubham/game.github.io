8faedbc65ff0485a614159834e80cce8f3c48472
shubham sanjay surve <87568048+surveshubham@users.noreply.github.com>
2022-01-06 21:56:28 +0530

    updated js for h3
    uploded new js folder

--- index.js
+++ index.js
@@ -32,13 +32,14 @@ function p1power(){
     p1totall = score
 
     if(p1totall <= 0){
-        alert("player1 won  press click player1 button to refresh player2 score")
+        alert("player1 won click player1 button to refresh player2 score")
         document.getElementById("p1s").innerHTML = p1win; 
         let wins = p1win + 1
         p1win = wins
         p1totall = 100
         if(p1win >= 4){
             alert("player1 wins restart")
+            document.getElementById("winner1").style.display = "block"
         }
     }
 }
@@ -52,13 +53,14 @@ function p2power(){
     p2totall = score
 
     if(p2totall <= 0){
-        alert("player2 won press click player2 button to refresh player1 score")
+        alert("player2 won click player2 button to refresh player1 score")
         document.getElementById("p2s").innerHTML = p2win; 
         let wins = p2win + 1
         p2win = wins
         p2totall = 100 
         if(p2win >= 4){
             alert("player2 wins restart")
+            document.getElementById("winner2").style.display = "block"
         }
     }
 }
